ac722ba9650281e9854a2e4d344f687bf9ab3544
Ignatious Rozario <98931238+Ignatious600@users.noreply.github.com>
2022-03-27 19:24:45 +0600

    Add files via upload

---
+++ Test/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Title");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(420,420);
+		frame.setVisible(true);
+		
+		ImageIcon image = new ImageIcon("boy_down_1.png");
+		frame.setIconImage(image.getImage());
+		
+		System.out.println("Hello jvae");
+	}
+
+}
